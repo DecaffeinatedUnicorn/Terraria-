20d905e69a835a49acb14024b1d82831f3931196
DecaffeinatedUnicorn <115904231+DecaffeinatedUnicorn@users.noreply.github.com>
2022-10-16 16:14:56 +1300

    Make vanilla npc drop item - attempt 1
    public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
            {
                // First, we need to check the npc.type to see if the code is running for the vanilla NPCwe want to change
                if (npc.type == NPCID.GreenSlime)
                {
                    // This is where we add item drop rules for VampireBat, here is a simple example:
                    npcLoot.Add(ItemDropRule.Common(ItemID.Shackle, 50));
                }
                // We can use other if statements here to adjust the drop rules of other vanilla NPC
            }
    
    Problem here

---
+++ TerrariaPlusPlusPlus/Global/GlobalItems/GlobalItemList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace TerrariaPlusPlusPlus.Global.Items
+{
+
+    public class GlobalItemList : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+
+            item.autoReuse = true;  // makes all items auto-reuse
+
+
+            if (item.type == ItemID.MagicMissile)
+            {
+                item.damage = 9999;
+                item.useAnimation = 1;
+                item.mana = 1;       // makes magic missile absurdly overpowered  -  remember to turn this off when actually playing - example for how to modify vanilla weapons
+            }
+        }
+
+    }
+
+}
---
+++ TerrariaPlusPlusPlus/Global/GlobalNPC/GlobalNPCList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
+
+namespace TerrariaPlusPlusPlus.Global.NPC
+{
+    public class GlobalNPCList : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            // First, we need to check the npc.type to see if the code is running for the vanilla NPCwe want to change
+            if (npc.type == NPCID.GreenSlime)
+            {
+                // This is where we add item drop rules for VampireBat, here is a simple example:
+                npcLoot.Add(ItemDropRule.Common(ItemID.Shackle, 50));
+            }
+            // We can use other if statements here to adjust the drop rules of other vanilla NPC
+        }
+    }
+
+
+}
